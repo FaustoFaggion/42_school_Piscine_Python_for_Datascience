deebd88cc3a184d257f50ef39180101719efc3eb
FaustoFaggion <faustofaggion@hotmail.com>
2024-09-25 15:19:08 -0300

    ex00 and ex02

--- ex02/find_ft_type.py
+++ ex02/find_ft_type.py
@@ -1,4 +1,13 @@
 
 def all_thing_is_obj(object: any) -> int:
-        print(type(object))
+        
+    name = type(object).__name__
+    if (name == "str"):
+        print(object, 'is in the ktchen :', type(object))
         return 42
+    if (name == "int"):
+        print("Type not found")
+        return 42
+     
+    print(name.capitalize(), ":", type(object))
+    return 42

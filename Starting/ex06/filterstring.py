696a8fc5ad76ccfd5ba0233990b2081b92526b0e
FaustoFaggion <faustofaggion@hotmail.com>
2024-10-02 09:19:40 -0300

    Starting list. ex06 correction

--- Starting/ex06/filterstring.py
+++ Starting/ex06/filterstring.py
@@ -1,5 +1,40 @@
+import sys
+from ft_filter import ft_filter
 
-def filterstring(text: list, n: int):
+def f(x):
+    if len(x) < 3:
+       return True
+    else:
+       return False
 
-    return [x for x in text if len(x) > n]
+def main(argv: any):
+    
+    # w = filter(lambda word: len(word) > int(argv[2]), argv[1])
+    
+    # for i in w:
+    #     print(i)
+
+    try:
+        assert len(argv) == 3, "AssertionError: the arguments are bad"
+        assert argv[2].isdigit(), "AssertionError: the arguments are bad .."
+        
+        n = int(argv[2])
+        text_to_list = argv[1].split()
+        for item in text_to_list:
+            assert item.isalpha(), "AssertionError: the arguments are bad ."
+    
+        words_lst: list = [x for x in text_to_list if x.isalpha()]        
+        response = ft_filter(lambda word: len(word) > n, words_lst)
+   
+        for x in response:
+            print(x)
+
+    except Exception as e:
+        print(e)
+
+if __name__ == "__main__":
+    main(sys.argv)
+
+
+
     

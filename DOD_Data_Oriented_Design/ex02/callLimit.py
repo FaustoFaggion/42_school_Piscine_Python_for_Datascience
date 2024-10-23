21e2c4616f38227e5f796c8c09f93a355eb94e13
Fausto <faustofaggion@hotmail.com>
2024-10-23 18:36:02 -0300

    DOD_Data_Oriented_Design/ex02 done

--- DOD_Data_Oriented_Design/ex02/callLimit.py
+++ DOD_Data_Oriented_Design/ex02/callLimit.py
@@ -3,7 +3,13 @@ def callLimit(limit: int):
     def callLimiter(function):
 
         def limit_function(*args: any, **kwds: any):
-            return print("haha")
+            nonlocal count
+            if count >= limit:
+                print("Error: call too many times")
+                return
+            function()
+            count += 1
+            print("count ", count)
     
         return limit_function
     
